59966200684e39693641238c5fbd5b1da38dab86
rustemdev <rustemnew@icloud.com>
2025-06-10 22:42:07 +0500

    1.0.0

---
+++ src/components/MapView.tsx
@@ -0,0 +1,35 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Импорты иконок
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Настройка иконки по умолчанию
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const MapView = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: '500px', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>Привет из Leaflet</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapView;
